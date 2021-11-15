bdba91e662c82df796259f397afd3e3a88bf8550
Talita Beneves <talitabeneves15@gmail.com>
2021-11-15 07:58:47 -0300

    Header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -22,8 +22,7 @@ const Navbar = () => {
           <img src={logo} alt="logo"/>
         </div>
         <div className="gpt3__navbar-links_container">
-        <Menu />
-
+          <Menu />
         </div>
       </div>
       <div className="gpt3__navbar-sign">
@@ -39,10 +38,10 @@ const Navbar = () => {
           <div className="gpt3__navbar-menu_container scale-up-center">
             <div className="gpt3__navbar-menu_container-links">
               <Menu />
-                <div className="gpt3__navbar-menu_container-links-sign">
-                  <p>Sign in</p>
-                  <button type="button">Sign up</button>
-                </div>
+            </div>
+            <div className="gpt3__navbar-menu_container-links-sign">
+              <p>Sign in</p>
+              <button type="button">Sign up</button>
             </div>
           </div>
         )}
