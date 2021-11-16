2c4801170c05515b4395b333af918c0204d83f4f
Talita Beneves <talitabeneves15@gmail.com>
2021-11-16 15:13:11 -0300

    Blog Section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import Article from '../../components/article/Article';
+import { pic, pic6, pic7, pic8, pic9 } from './imports';
 import './blog.css';
 
 
 const Blog = () => {
   return (
-    <div>
-      
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article imgUrl={pic} date="Nov 16, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article imgUrl={pic6} date="Nov 16, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imgUrl={pic7} date="Nov 16, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imgUrl={pic8} date="Nov 16, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imgUrl={pic9} date="Nov 16, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/containers/blog/imports.js
@@ -0,0 +1,13 @@
+import pic from "../../assets/pic.png";
+import pic6 from "../../assets/pic6.jpg";
+import pic7 from "../../assets/pic7.jpg";
+import pic8 from "../../assets/pic8.jpg";
+import pic9 from "../../assets/pic9.jpg";
+
+export {
+  pic, 
+  pic6, 
+  pic7, 
+  pic8, 
+  pic9, 
+}
