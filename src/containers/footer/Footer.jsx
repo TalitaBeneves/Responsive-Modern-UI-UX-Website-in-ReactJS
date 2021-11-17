cf77bd994b40fd0ec3313614d5415369d9e8ec57
Talita Beneves <talitabeneves15@gmail.com>
2021-11-16 23:55:39 -0300

    Footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,13 +1,53 @@
 import React from 'react';
+import logo from '../../assets/logo.svg';
+import footer from '../../assets/ai.jpg';
 import './footer.css';
 
 
 const Footer = () => {
   return (
-    <div>
+    <div className="gpt3__footer">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others</h1>
+      </div>
       
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions </p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Rio de Janeiro</p>
+          <p>21 990497280</p>
+          <p>talitabeneves15@gmail.com</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p>©2021 Talita Beneves. All rights reserved.</p>
+      </div>
+      <div className="bg-image">
+        <img src={footer} alt="footer" />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer;
